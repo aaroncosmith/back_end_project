21448b7fe8f197366cb95dae29e1f94820569bd4
Alan Jordan <alan.jordanbiz@gmail.com>
2020-03-12 11:14:41 -0400

    updated model functions

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,39 +1,57 @@
-const db = require('./conn')
+const db = require("./conn");
 
+class PictureReviewModel{
+  constructor(id, picture) {
+    this.id = id;
+    this.picture = picture;
+  }
 
-class getImageDisplay {
-    constructor(id, picture_id) {
-        this.id = id;
-        this.picture_id = picture_id;
-        
+  static async getAllPictures() {
+    try {
+      const response = await db.any(`SELECT * FROM images;`);
+      return response;
+    } catch (error) {
+      console.error("ERROR: ", error);
+      return error;
     }
-    static async pushImg(url) {
-        try {
-            const response = await db.one('INSERT INTO images (picture) VALUES ($1) RETURNING id;', [url]);
-    return response
-        } catch (error) {
-            console.error('ERROR', error);
-            return error
-        }
+  }
+
+  static async getPicturesById(r_id) {
+    try {
+      const response = await db.any(
+        `SELECT * FROM images WHERE id = ${r_id};`
+      );
+      return response;
+    } catch (error) {
+      console.error("ERROR: ", error);
+      return error;
     }
-    static async renderImage() {
-        try {
-            const response = await db.any(`SELECT * FROM images;`);
-            return response;
-        } catch (error) {
-            console.error('ERROR', error);
-            return error;
-        }
+  }
+
+  static async getCommentsByImageId(r_id) {
+    try {
+      const response = await db.any(
+        `SELECT * FROM comments WHERE images_id = ${r_id};`
+      );
+      return response;
+    } catch (error) {
+      console.error("ERROR: ", error);
+      return error;
     }
-    static async getById(picture) {
-        try {
-            const response = await db.one(`SELECT * FROM images WHERE id = ${picture}`);
-            return response;
-        } catch (error) {
-            console.error('ERROR', error)
-            return error;
-        }
+  }
+  static async addComment(r_id, comment) {
+    try {
+      const response = await db.one(
+        `INSERT INTO comments (user_id, picture_id, comment) VALUES ($1, $2, $3) RETURNING id`,
+        [r_id, review_title, review_text]
+      );
+
+      return response;
+    } catch (error) {
+      console.log("ERROR: ", error);
+      return error;
     }
+  }
 }
 
-module.exports = getImageDisplay;
+module.exports = PictureReviewModel;

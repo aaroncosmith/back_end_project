0c8189811aa6ab9c60e7a67b1459e9b592e9d01a
Alan Jordan <alan.jordanbiz@gmail.com>
2020-03-12 11:16:29 -0400

    changed functions

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ imageModel = require('../models/imageModel');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const resultData = await imageModel.renderImage();
+  const resultData = await imageModel.getAllPictures();
   res.render('template', {
     locals: {
       title: 'Film Data',
       resultData: resultData,
-      is_logged_in: req.session.is_logged_in
+      is_logged_in: req.session.is_logged_in,
+      name: req.session.name
     },
     partials: {
       partial: 'partial-index'
